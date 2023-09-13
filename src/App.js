b58f85f0ef9fe58b761e5624d8910908dfd27f11
Kipland Melton <kiplandmelton@gmail.com>
2023-09-12 20:30:11 -0500

    removed unnecessary extra line from imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from './Home';
 import Resume from './Resume';
